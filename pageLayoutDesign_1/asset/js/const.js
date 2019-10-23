d5fc0450413640d372080bb35e18a4f391460d43
MYxu <1977492949@qq.com>
2019-10-23 19:48:54 +0800

    切换导航加载不同内容页面时，动态加载对应的js、css文件，并实现静态资源的版本管理

---
+++ pageLayoutDesign_1/asset/js/const.js
@@ -0,0 +1,11 @@
+// 项目根路径
+const baseUrl = window.location.href.slice(0, window.location.href.lastIndexOf('/'));
+
+// 项目静态资源根路径
+const assetBaseUrl = baseUrl + '/asset/';
+const cssBaseUrl = assetBaseUrl + 'css/';
+const jsBaseUrl = assetBaseUrl + 'js/';
+
+
+// 静态资源文件的更新版本
+const version = 2;
--- pageLayoutDesign_1/asset/js/index.js
+++ pageLayoutDesign_1/asset/js/index.js
@@ -1,6 +1,5 @@
-let currNavKey = 'home';
-function navBarSwitch(key) {
-    console.log(key);
+function navBarSwitch(currNavKey) {
+    console.log(currNavKey);
     let middleContainerMinHeight = window.innerHeight - 200,
         loadingAnimateContainerEl = $('.loading-animate-container'),
         contentContainerEl = $('.content-container');
@@ -9,11 +8,26 @@ function navBarSwitch(key) {
         home: 'home.html',
         gameCenter: 'gameCenter.html'
     };
+    let scriptList = {
+        home: jsBaseUrl + 'home.js',
+        gameCenter: jsBaseUrl + 'gameCenter.js'
+    };
+    let linkList = {
+        home: cssBaseUrl + 'home.css',
+        gameCenter: cssBaseUrl + 'gameCenter.css'
+    };
+
     contentContainerEl.hide();
     contentContainerEl.empty();
     loadingAnimateContainerEl.fadeIn(500);
 
-    contentContainerEl.load(contentList[key], function () {
+    // 加载对应的js、css文件
+    console.log(importJs(scriptList[currNavKey]));
+    console.log(importCss(linkList[currNavKey]));
+
+    // 加载页面结构
+    contentContainerEl.load(contentList[currNavKey], function () {
+        // 模拟3s后获取到页面内容信息
         setTimeout(function () {
             loadingAnimateContainerEl.fadeOut(500, function () {
                 loadingAnimateContainerEl.hide();
@@ -21,6 +35,35 @@ function navBarSwitch(key) {
                 contentContainerEl.fadeIn(500);
                 console.log('页面切换成功');
             });
-        }, 10000)
+        }, 3000)
     });
+}
+
+/**
+ * 动态加载指定的js文件
+ * @param fileSrc
+ * @returns {boolean}
+ */
+function importJs(fileSrc = '') {
+    let scriptElementList = $('script');
+    for (let i = 0; i < scriptElementList.length; i++) {
+        if (scriptElementList[i].src.indexOf(fileSrc) !== -1) {
+            // 该js文件已经引入
+            return false;
+        }
+    }
+    $.getScript(fileSrc);
+    return true;
+}
+
+function importCss(fileSrc = '') {
+    console.log(fileSrc);
+    let linkElementList = $('link');
+    for (let i = 0; i < linkElementList.length; i++) {
+        if (linkElementList[i].href.indexOf(fileSrc) !== -1) {
+            return false;
+        }
+    }
+    $('head').append('<link rel="stylesheet" type="text/css" href="' + fileSrc + '?v=' + version +'">');
+    return true;
 }
